926975c64b930b5142f19a808152ace1d85b0599
Salo018 <salome.rivas@uao.edu.co>
2026-05-20 12:22:34 -0500

    Script con lectura de los archivos, convertidos a df y verificacion

---
+++ script_proyecto_final_EYP2.R
@@ -0,0 +1,56 @@
+# Instalar librerías solo si no están instaladas
+install.packages("ggplot2")    # Visualización
+install.packages("naniar")     # Datos faltantes
+install.packages("corrplot")   # Matriz de correlaciones
+install.packages("dplyr")      # Manejo de datos
+install.packages("ggcorrplot") # Visualización
+install.packages("readxl")     # Lectura de Excel
+
+# Cargar librerías
+library(ggplot2)
+library(naniar)
+library(corrplot)
+library(ggcorrplot)
+library(readr)
+library(purrr)
+library(dplyr)
+library(stringr)
+library(readxl)
+
+# Carga de datos desde GitHub
+
+# URL de los archivos
+url_viviendas <- "https://raw.githubusercontent.com/Salo018/Proyecto3-EYP2/main/data/Viviendas.csv"
+url_punto1 <- "https://raw.githubusercontent.com/Salo018/Proyecto3-EYP2/main/data/Punto_1.xlsx"
+url_consumo <- "https://raw.githubusercontent.com/Salo018/Proyecto3-EYP2/main/data/Consumo.xlsx"
+
+# Leer archivos csv
+viviendas <- read.csv(
+  url_viviendas,
+  sep = ";"
+)
+
+# Funcion para leer los archivos xlsx desde github
+leer_excel <- function(url, sheet = 1) {
+  temp <- tempfile(fileext = ".xlsx")
+  download.file(
+    url,
+    destfile = temp,
+    mode = "wb"
+  )
+  read_excel(temp, sheet = sheet)
+}
+
+# Leer los archivos excel
+punto1 <- leer_excel(url_punto1)
+consumo <- leer_excel(url_consumo)
+
+# Convertir a Dataframe 
+df.viviendas <- as.data.frame(viviendas)
+df.punto1 <- as.data.frame(punto1)
+df.consumo <- as.data.frame(consumo)
+
+# Verificación
+head(df.viviendas)
+head(df.punto1)
+head(df.consumo)
